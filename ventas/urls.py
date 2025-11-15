281e1bf1956c7ff0531b835509c159d4a39273b1
Tomas <tomas.luque@itscordoba.edu.ar>
2025-11-15 12:56:37 -0300

    se agrega el grafico de ventas x dia con chart.js y boton de grafico de ventas

--- ventas/urls.py
+++ ventas/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.VentaListView.as_view(), name='ventas_lista'),
     path('nuevo/', views.nueva_venta, name='ventas_nueva'),
+    path('grafico/', views.VentasPorDiaView.as_view(), name='ventas_grafico'),
     path('borrar/<int:pk>/', views.VentaDeleteView.as_view(), name='ventas_borrar'),
     path('<int:pk>/', views.VentaDetalleView.as_view(), name='ventas_detalle'),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic import ListView, DetailView, DeleteView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import login_required, permission_required
 from django.urls import reverse_lazy
 from django.db import transaction
+from django.db.models import Sum
+from django.db.models.functions import TruncDate
+import json
 
 from .models import Venta
 from .forms import VentaForm, ItemVentaFormSet
@@ -32,6 +35,29 @@ class VentaDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     template_name = 'ventas/confirmar_borrado.html'
     success_url = reverse_lazy('ventas_lista')
     permission_required = "ventas.delete_venta"
+
+
+class VentasPorDiaView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
+    template_name = 'ventas/grafico_ventas.html'
+    permission_required = "ventas.view_venta"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        ventas_por_dia = (
+            Venta.objects
+            .annotate(fecha_dia=TruncDate('fecha'))
+            .values('fecha_dia')
+            .annotate(total_dia=Sum('total'))
+            .order_by('fecha_dia')
+        )
+
+        labels = [v['fecha_dia'].strftime('%Y-%m-%d') for v in ventas_por_dia]
+        data = [float(v['total_dia']) for v in ventas_por_dia]
+
+        context['labels'] = json.dumps(labels)
+        context['data'] = json.dumps(data)
+        return context
 
 
 @login_required
